8812f41d5a6d93210cf0ab585bb180160819f4b3
linah ebrahim alsayid <s445002548@uqu.edu.sa>
2026-05-08 22:25:05 +0300

    delete the code

--- src/training.py
+++ src/training.py
@@ -1,10 +1,1 @@
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
 
-def train_model(df):
-    # دانة، هنا تضعين كود الـ Decision Tree وتقسيم البيانات
-    print("🌳 Training Decision Tree...")
-    # X = ... , y = ...
-    # model = DecisionTreeClassifier()
-    # return model, X_test, y_test
-    return None, None, None
