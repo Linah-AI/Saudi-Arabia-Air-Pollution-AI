7f475e9f70ff587a4f9c01754694b7334e076728
Danah Alharbi <S445000673@uqu.edu.sa>
2026-05-09 21:11:17 +0300

    Update training.py

--- src/training.py
+++ src/training.py
@@ -32,6 +32,6 @@ def train_model(df):
     # تدريب الموديل
     model.fit(X_train, y_train)
 
-    print("✅ Model Trained Successfully")
+    print("Model Trained Successfully")
 
     return model, X_test, y_test
